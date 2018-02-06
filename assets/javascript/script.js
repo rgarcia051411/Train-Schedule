a20d0ab4b071fe1769b55e9c9a8c183763abe496
Robinson Garcia <robinson@Robinsons-MacBook-Pro.local>
2018-02-05 23:30:43 -0800

    I added mp3 folder and coded moment JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -40,8 +40,8 @@ var config = {
   // Declare Initial Values
   var trainName = "";
   var destination ="";
-  var trainTime = "";
-  var trainFrequency = "";
+  var firstTrain = "";
+  var frequency = "";
 
    $(document).ready(function(){
 
@@ -52,34 +52,64 @@ var config = {
    	// Grab the value and store in the declared variables.
    	trainName = $("#train-name-input").val().trim();
    	destination = $("#destination-input").val().trim();
-   	trainTime = $("#train-time-input").val().trim();
-   	trainFrequency = $("#train-frequency-input").val().trim();
+   	firstTrain = $("#train-time-input").val().trim();
+   	frequency = $("#train-frequency-input").val().trim();
 
 
    		// Push data into Firebase
    		dataRef.ref().push({
    			train: trainName,
    			destination: destination,
-   			time: trainTime,
-   			frequency: trainFrequency,
+   			firstTrain: firstTrain,
+   			frequency: frequency,
    			dateAdded: firebase.database.ServerValue.TIMESTAMP
 
    		});
    	});
 
-   	// Firbase watcher
+   	// Firbase watcher and child added for data
    	dataRef.ref().on("child_added",  function(childSnapshot){
 
    		// Console log everything from snapshot.
    		console.log(childSnapshot.val().train);
    		console.log(childSnapshot.val().destination);
-   		console.log(childSnapshot.val().time);
+   		console.log(childSnapshot.val().firstTrain);
    		console.log(childSnapshot.val().frequency);
+
+   		// moment JS
+   		var timeHour = moment().format('H');
+   		var timeMin = moment().format('m');
+   		var firstTrainHour = moment(firstTrain, "HH:mm").format('H');
+   		var firstTrainMin = moment(firstTrain, "HH:mm").format('m');
+
+   		var firstTrainMoment = (firstTrainHour * 60) + (firstTrainMin * 1);
+   		var timeMoment = (timeHour * 60) + (timeMin * 1);
+
+   		// How much time elapsed since the first train?
+   		var timeDiff = timeMoment - firstTrainMoment;
+
+   		//Number of trains have passed
+   		var trainSinceFirst = Math.floor(timeDiff/frequency);
+
+   		// How long till the next train
+   		var nextTrainArrival = ((trainSinceFirst + 1) * frequency) + firstTrainMoment;
+
+   		// values for Minutes away and next arrival
+   		if( firstTrainMoment < timeMoment ){
+   			var minutesAway = nextTrainArrival - timeMoment;
+   			var nextTrainArrival = moment().add(minutesAway, 'minutes').format('HH:mm');
+
+   		}
+   		else{
+   			var nextTrainArrival = firstTrain
+   			var minutesAway = firstTrainMoment - timeMoment;
+   			// console.log(minAway);
+   		};
 
    		// Append inputs in the Train Schedule Panel
    		var table = $("tbody");
 
-   		table.append("<tr><td>" + (childSnapshot.val().train) + "</td>" + "<td>" +(childSnapshot.val().destination) + "<td>" + (childSnapshot.val().frequency) + "</td></tr>" 
+   		table.append("<tr><td>" + (childSnapshot.val().train) + "</td>" + "<td>" +(childSnapshot.val().destination) + "<td>" + (childSnapshot.val().frequency) + "<td>"+ nextTrainArrival + "</td>" + "<td>" + minutesAway + "</td></tr>" 
    			);
 
 
