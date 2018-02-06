999915ce83d16393d988388963d406a439aaf2fb
Robinson Garcia <robinson@Robinsons-MacBook-Pro.local>
2018-02-05 21:48:56 -0800

    added a clock feature into my code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,30 @@
-// Initialize Firebase
+// code for clock
+function clock(){
+var fullDate = new Date();
+var hours = fullDate.getHours();
+var minutes = fullDate.getMinutes();
+var seconds = fullDate.getSeconds();
 
+if (hours < 10) {
+	hours = "0" + hours;
+}
+if (minutes < 10) {
+	minutes = "0" + minutes;
+}
+if (seconds < 10) {
+	seconds = "0" + seconds;
+}
+
+document.getElementById('hour').innerHTML = hours;
+document.getElementById('minute').innerHTML = ":" + minutes;
+document.getElementById('second').innerHTML =":" + seconds;
+
+}
+
+setInterval(clock,100);
+
+
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyDW-08jWFN9XsoMseHaNgmzFQIJEMAMz68",
     authDomain: "train-schedule-eeb4a.firebaseapp.com",
