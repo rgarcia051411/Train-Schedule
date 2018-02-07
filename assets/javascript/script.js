c6cd92e9b3300cb9f4a0144919adc9f98accf662
Robinson Garcia <robinson@Robinsons-MacBook-Pro.local>
2018-02-06 22:14:52 -0800

    made some changes to my js code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -50,37 +50,39 @@ var config = {
    		event.preventDefault();
 
    	// Grab the value and store in the declared variables.
-   	trainName = $("#train-name-input").val().trim();
-   	destination = $("#destination-input").val().trim();
-   	firstTrain = $("#train-time-input").val().trim();
-   	frequency = $("#train-frequency-input").val().trim();
+     	trainName = $("#train-name-input").val().trim();
+     	destination = $("#destination-input").val().trim();
+     	firstTrain = $("#train-time-input").val().trim();
+     	frequency = $("#train-frequency-input").val().trim();
 
 
-   		// Push data into Firebase
-   		dataRef.ref().push({
+     		// Push data into Firebase
+     	dataRef.ref().push({
    			train: trainName,
    			destination: destination,
    			firstTrain: firstTrain,
    			frequency: frequency,
    			dateAdded: firebase.database.ServerValue.TIMESTAMP
-
-   		});
+   		 });
    	});
 
    	// Firbase watcher and child added for data
    	dataRef.ref().on("child_added",  function(childSnapshot){
 
    		// Console log everything from snapshot.
-   		console.log(childSnapshot.val().train);
-   		console.log(childSnapshot.val().destination);
-   		console.log(childSnapshot.val().firstTrain);
-   		console.log(childSnapshot.val().frequency);
+   		// console.log(childSnapshot.val().train);
+   		// console.log(childSnapshot.val().destination);
+   		// console.log(childSnapshot.val().firstTrain);
+   		// console.log(childSnapshot.val().frequency);
 
+      var firstTrainV = childSnapshot.val().firstTrain;
+      var frequencyV = childSnapshot.val().frequency;
+      
    		// moment JS
    		var timeHour = moment().format('H');
    		var timeMin = moment().format('m');
-   		var firstTrainHour = moment(firstTrain, "HH:mm").format('H');
-   		var firstTrainMin = moment(firstTrain, "HH:mm").format('m');
+   		var firstTrainHour = moment(firstTrainV, "HH:mm").format('H');
+   		var firstTrainMin = moment(firstTrainV, "HH:mm").format('m');
 
    		var firstTrainMoment = (firstTrainHour * 60) + (firstTrainMin * 1);
    		var timeMoment = (timeHour * 60) + (timeMin * 1);
@@ -89,23 +91,28 @@ var config = {
    		var timeDiff = timeMoment - firstTrainMoment;
 
    		//Number of trains have passed
-   		var trainSinceFirst = Math.floor(timeDiff/frequency);
+   		var trainSinceFirst = Math.floor(timeDiff/frequencyV);
 
    		// How long till the next train
-   		var nextTrainArrival = ((trainSinceFirst + 1) * frequency) + firstTrainMoment;
-
+   		var nextTrainArrival = ((trainSinceFirst + 1) * frequencyV) + firstTrainMoment;
+      var minutesAway;
+      var nextTrainArrival;
    		// values for Minutes away and next arrival
    		if( firstTrainMoment < timeMoment ){
-   			var minutesAway = nextTrainArrival - timeMoment;
-   			var nextTrainArrival = moment().add(minutesAway, 'minutes').format('HH:mm');
+   			minutesAway = nextTrainArrival - timeMoment;
+   			nextTrainArrival = moment().add(minutesAway, 'minutes').format('HH:mm');
 
    		}
    		else{
-   			var nextTrainArrival = firstTrain
-   			var minutesAway = firstTrainMoment - timeMoment;
+   			nextTrainArrival = firstTrainV
+   			minutesAway = firstTrainMoment - timeMoment;
    			// console.log(minAway);
    		};
 
+
+
+      console.log("nextTrainArrival", nextTrainArrival);
+      console.log("minutesAway", minutesAway)
    		// Append inputs in the Train Schedule Panel
    		var table = $("tbody");
 
@@ -115,7 +122,7 @@ var config = {
 
    	// Error handler
    	},function(errorObjects){
-   		console.log("Errors handled:" + errorObjects.code)
+   		//console.log("Errors handled:" + errorObjects.code)
 
    	});
 
